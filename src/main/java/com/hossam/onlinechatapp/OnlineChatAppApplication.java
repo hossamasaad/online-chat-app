ed82a85fef81b21970fb1a64b263dbf3eadf023f
hossamasaad <hossamasaad10@gmail.com>
2023-09-17 23:03:17 +0300

    Start Online Chat Project

---
+++ src/main/java/com/hossam/onlinechatapp/OnlineChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.hossam.onlinechatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineChatAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hossam/onlinechatapp/OnlineChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hossam.onlinechatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
